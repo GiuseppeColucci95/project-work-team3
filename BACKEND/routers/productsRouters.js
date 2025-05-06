c57d5084e482550f7806e5792593776c9d32c821
PirasSalvatore <torep.92@gmail.com>
2025-05-06 18:39:20 +0200

    add route for most recent and route for most sold

--- BACKEND/routers/productsRouters.js
+++ BACKEND/routers/productsRouters.js
@@ -1,27 +1,39 @@
 const express = require("express")
 const router = express.Router()
 
-//index (read)
+//index
 router.get("/", (req, res) => {
     console.log("Hello from the products router");
     res.json({ message: "Hello from the products router" })
 })
 
-//router for products list filtered by tag(pathologies)
+//route for products list filtered by tag(pathologies)
 router.get("/pathologies/:tag", (req, res) => {
     const { tag } = req.params
     console.log(tag)
     res.json({ message: `Hello from the products router with tag ${tag}` })
 })
 
-//router for products list filtered by category
+//route for products list filtered by category
 router.get("/categories/:category", (req, res) => {
     const { category } = req.params
     console.log(category)
     res.json({ message: `Hello from the products router with category ${category}` })
 })
 
-//router for product details
+//route for products list filtered by most recent
+router.get("/recent", (req, res) => {
+    console.log("recent")
+    res.json({ message: "Hello from the products router with recent" })
+})
+
+//route for products list filtered by most sold
+router.get("/mostsold", (req, res) => {
+    console.log("most sold")
+    res.json({ message: "Hello from the products router with most sold" })
+})
+
+//route for product details
 router.get("/product/:slug", (req, res) => {
     const { slug } = req.params
     console.log(slug)
