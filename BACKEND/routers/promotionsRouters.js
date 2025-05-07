38877523d39b66718628c67b5b38d72aeb6f3777
PirasSalvatore <torep.92@gmail.com>
2025-05-07 11:41:56 +0200

    feat: implement promotions controller and update promotions router to use controller

--- BACKEND/routers/promotionsRouters.js
+++ BACKEND/routers/promotionsRouters.js
@@ -1,13 +1,11 @@
 //imports
 const express = require("express")
 const promotionsRouters = express.Router()
+const promotionsController = require("../controllers/PromotionsController")
 
 //routes
 
 //index
-promotionsRouters.get("/", (req, res) => {
-    console.log("Hello from the promotions router")
-    res.json({ message: "Hello from the promotions router" })
-})
+promotionsRouters.get("/", promotionsController.index)
 
 module.exports = promotionsRouters
