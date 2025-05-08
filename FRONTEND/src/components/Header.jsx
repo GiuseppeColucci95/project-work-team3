e589c74063c7b2d3b6cfe738c67b9ff3bfb31c8e
antonella1986 <antonella.leo.mail@gmail.com>
2025-05-08 12:45:58 +0200

    edit header

--- FRONTEND/src/components/Header.jsx
+++ FRONTEND/src/components/Header.jsx
@@ -6,7 +6,28 @@ export default function Header() {
   return (
     <>
       <header>
-        <h1>HEADER</h1>
+        <div className="menu-sx">
+          <img src="https://picsum.photos/50/50" alt="" />
+          <ul className="menu">
+            <li>Menu</li>
+            <li>Products</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+        <div className="menu-ce">
+          <form className="onSubmit">
+            <input
+              type="text"
+              className="searchbar"
+              placeholder="Search your products"
+            />
+            <button type="submit" className="menu-icons search"><img className="icon-search" src="/img/search.svg" alt="" /></button>
+          </form>
+        </div>
+        <div className="menu-dx">
+          <img className="menu-icons" src="/img/favourites.svg" alt="" />
+          <img className="menu-icons" src="/img/cart.svg" alt="" />
+        </div>
       </header>
     </>
   );
