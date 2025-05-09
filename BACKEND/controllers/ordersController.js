aabe943069e9cad736bff50e0e3634a69a916eaf
PirasSalvatore <torep.92@gmail.com>
2025-05-09 14:51:31 +0200

    fix quesry insert orders, create query insert orders prodact

--- BACKEND/controllers/ordersController.js
+++ BACKEND/controllers/ordersController.js
@@ -3,6 +3,8 @@ const { orderValidate, userValidate } = require('../functions/validate.js')
 
 //connection to the database
 
+const connection = require('../data/db')
+
 //funtions to orders controller
 
 
@@ -11,8 +13,11 @@ function create(req, res) {
     const order = req.body
 
     //query to insert an element into orders tables
-    const sql = `INSERT INTO orders (id, promotion_id, firstname, lastname, mail, phone, address, total_not_discounted, total_discounted, shipping, final_price, status)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const insertOrder = `INSERT INTO orders (promotion_id, firstname, lastname, mail, phone, address, total_not_discounted, total_discounted, shipping, final_price, status, created_at)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`
+
+    const insertOrderProduct = `INSERT INTO order_product (order_id, product_id, quantity)
+    VALUES (?, ?, ?)`
 
     const { promotion_id,
         total_not_discounted,
@@ -21,13 +26,11 @@ function create(req, res) {
         final_price,
         status,
         products,
-        first_name,
-        last_name,
+        firstname,
+        lastname,
         phone,
         mail,
         address } = order
-
-
 
     //validate order data
     const errorOrderList = orderValidate(order)
@@ -42,8 +45,32 @@ function create(req, res) {
 
     // variable of query
 
-    console.log(`Creating order for user ${order.first_name} with data: `, order)
-    res.json({ message: `Creating order for user ${first_name} with data: ${order}` })
+    connection.query(insertOrder, [promotion_id, firstname, lastname, mail, phone.replaceAll(' ', ''), address, total_not_discounted, total_discounted, shipping, final_price, status], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed insert order' })
+
+        const order_id = results.insertId
+
+        // Crea un array di Promises per inserire i prodotti
+        //le promiss sono oggetti che mi permettono di gestire funzioni asincrone
+        const insertProductPromises = products.map(product => {
+            return new Promise((resolve, reject) => {
+                connection.query(insertOrderProduct, [order_id, product.product_id, product.quantity], (err, result) => {
+                    if (err) return reject(err)
+                    resolve(result.insertId)
+                })
+            })
+        })
+
+        // Attendi che tutte le query siano completate
+        // con la funzione promise.all ciclo all'intero dell'array di promise per andare a gestire ogni singola promise
+        Promise.all(insertProductPromises)
+            .then(inseredOrderProduct => {
+                res.status(201).json({ message: 'order and order_products created successfully ', orderId: order_id })
+            })
+            .catch(() => {
+                res.status(500).json({ error: 'Database query failed insert order_products' })
+            })
+    })
 
 }
 
