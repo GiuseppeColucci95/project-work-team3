8db455ed4a7446df4326c6bf8d2a196d25562790
antonella1986 <antonella.leo.mail@gmail.com>
2025-05-10 17:16:24 +0200

    add allergen icons

--- FRONTEND/src/pages/Homepage.jsx
+++ FRONTEND/src/pages/Homepage.jsx
@@ -17,9 +17,15 @@ export default function Homepage() {
             Don't <br />compromise.
           </p>
           <button className="explore-btn">EXPLORE OUR PRODUCTS</button>
-
         </div >
       </section>
+      <div className="allergens">
+        <img src="img/gluten-free.png" className="food-allergens" alt="gluten-free" />
+        <img src="img/sugar-free.png" className="food-allergens" alt="sugar-free" />
+        <img src="img/lactose-free.png" className="food-allergens" alt="lactose-free" />
+        <img src="img/nuts-free.png" className="food-allergens" alt="nuts-free" />
+        <img src="img/nickel-free.png" className="food-allergens" alt="nickel-free" />
+      </div>
       {/* JUMBOTRON SECTION */}
 
       <section id="description" className="my-5">
@@ -29,7 +35,7 @@ export default function Homepage() {
               <img src="https://picsum.photos/400/300" alt="description image" className="w-100" />
             </div>
             <div className="col-sm-12 col-md-12 col-lg-6">
-              <p>
+              <p className="description-text">
                 <strong>Eat Your Way</strong> is your go-to online destination for delicious, safe, and carefully curated food products designed for people with specific dietary needs. Whether you're managing diabetes, living with celiac disease, or navigating food intolerances, we make it easy to shop with confidence. <br />Our mission is to put health and flavor on the same plate. Because everyone deserves to enjoy food without compromise.
               </p>
             </div>
