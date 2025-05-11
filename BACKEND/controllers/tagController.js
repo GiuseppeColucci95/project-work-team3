04fd60bb937ff1cf47afb0b773d195f38167d1f9
PirasSalvatore <torep.92@gmail.com>
2025-05-11 18:52:41 +0200

    refactor: migliorare la struttura del tagController con spaziatura e commenti

--- BACKEND/controllers/tagController.js
+++ BACKEND/controllers/tagController.js
@@ -2,18 +2,32 @@
 
 const connection = require('../data/db')
 
+
+// function index
 function index(req, res) {
+    const sql = `SELECT * FROM tags`
 
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
 
+        res.json(results)
+    })
 }
 
+//show
 function getByName(req, res) {
 
     const sql = `SELECT * FROM tags
                 WHERE name = ?`
 
+    const tagName = req.params.tag
 
 
+    connection.query(sql, tagName, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+
+        res.json(results[0])
+    })
 }
 
 
