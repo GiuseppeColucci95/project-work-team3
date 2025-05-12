f32bdb744b68b260bf6aea8b546153f8b7e67d37
PirasSalvatore <torep.92@gmail.com>
2025-05-12 16:05:07 +0200

    feat: add additional fields to Checkout form for user email, address, and postal code

--- FRONTEND/src/pages/Checkout.jsx
+++ FRONTEND/src/pages/Checkout.jsx
@@ -5,6 +5,12 @@ export default function Checkout() {
 
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
+  const [userEmail, setUserEmail] = useState("")
+  const [street, setStreet] = useState("")
+  const [streetNumber, setStreetNumber] = useState("")
+  const [city, setCity] = useState("")
+  const [province, setProvince] = useState("")
+  const [postalCode, setPostalCode] = ("")
 
 
   return (
@@ -20,6 +26,7 @@ export default function Checkout() {
                 <div className="col-md-6">
                   <label htmlFor="firstName" className="form-label">First name</label>
                   <input type="text" className="form-control" id="firstName"
+                    placeholder="Marco"
                     value={firstName}
                     onChange={e => setFirstName(e.target.value)}
                     required />
@@ -31,11 +38,89 @@ export default function Checkout() {
                 <div className="col-md-6">
                   <label htmlFor="lastName" className="form-label">First name</label>
                   <input type="text" className="form-control" id="lastName"
+                    placeholder="Rossi"
                     value={lastName}
                     onChange={e => setLastName(e.target.value)}
                     required />
                   <div className="valid-feedback">
-                    Valid First name
+                    Valid Last name
+                  </div>
+                </div>
+
+                <div className="col-md-12">
+                  <label htmlFor="email" className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="email"
+                    aria-describedby="emailHelpId"
+                    placeholder="abc@mail.com"
+                    value={userEmail}
+                    onChange={e => setUserEmail(e.target.value)}
+                  />
+                  <small id="emailHelpId" className="form-text text-muted">
+                    example@email.com
+                  </small>
+                </div>
+
+                <div className="col-md-9">
+                  <label htmlFor="street" className="form-label">Street</label>
+                  <input type="text" className="form-control" id="street"
+                    placeholder="Via Roma"
+                    value={street}
+                    onChange={e => setStreet(e.target.value)}
+                    required />
+                  <div className="valid-feedback">
+                    Valid Street
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="streetNumber" className="form-label">Street Number</label>
+                  <input type="text" className="form-control" id="streetNumber"
+                    placeholder="123"
+                    value={streetNumber}
+                    onChange={e => setStreetNumber(e.target.value)}
+                    required />
+                  <div className="valid-feedback">
+                    Valid Street Number
+                  </div>
+                </div>
+
+                <div className="col-md-6">
+                  <label htmlFor="city" className="form-label">City</label>
+                  <input type="text" className="form-control" id="city"
+                    placeholder="Roma"
+                    value={city}
+                    onChange={e => setCity(e.target.value)}
+                    required />
+                  <div className="valid-feedback">
+                    Valid City
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="province" className="form-label">Province</label>
+                  <input type="text" className="form-control" id="province"
+                    placeholder="RO"
+                    value={province}
+                    onChange={e => setProvince(e.target.value)}
+                    required />
+                  <div className="valid-feedback">
+                    Valid Province
+                  </div>
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="postalCode" className="form-label">Postal Code</label>
+                  <input type="text" className="form-control" id="postalCode"
+                    placeholder="00100"
+                    value={postalCode}
+                    onChange={e => setPostalCode(e.target.value)}
+                    required />
+                  <div className="valid-feedback">
+                    Valid Postal Code
                   </div>
                 </div>
 
