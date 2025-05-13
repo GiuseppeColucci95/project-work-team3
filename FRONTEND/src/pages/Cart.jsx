5997c78f053a44ae7ee3a7076f8accf4c3ae6b7a
GiuseppeColucci95 <colucci539@gmail.com>
2025-05-13 23:10:26 +0200

    fix: correct shipping cost formatting in Cart component

--- FRONTEND/src/pages/Cart.jsx
+++ FRONTEND/src/pages/Cart.jsx
@@ -45,9 +45,9 @@ export default function Cart() {
             </div>
             <div className="col-4 d-flex flex-column justify-content-center">
               <h3>ORDER SUMMARY</h3>
-              <h5>Shipping: {(totalPrice > 39.99) ? ('0.00') : ('9,99')}€ </h5>
 
               <h5>Total products: {totalPrice.toFixed(2)}€ </h5>
+              <h5>Shipping: {(totalPrice > 39.99) ? ('0.00') : ('9.99')}€ </h5>
               <h5>Total order: {(totalPrice > 39.99) ? (totalPrice.toFixed(2)) : ((totalPrice + 9.99).toFixed(2))}€ </h5>
 
               <Link to="/checkout">
