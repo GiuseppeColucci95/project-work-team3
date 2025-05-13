19887272522b3031dc1bac5af96998cd95dfdc9a
antonella1986 <antonella.leo.mail@gmail.com>
2025-05-13 18:29:32 +0200

    implement order confirmation page style

--- FRONTEND/src/pages/OrderConfirmation.jsx
+++ FRONTEND/src/pages/OrderConfirmation.jsx
@@ -7,19 +7,21 @@ export default function OrderConfirmation() {
 
   return (
     <>
-      <div className="container text-center py-4">
-        <img src="img/logo.png" alt="qualcosa di strano" />
-
-        <p className="fw-bold">
-          Your order is placed
-        </p>
-        <p>
-          Check your mail for confirmation
-        </p>
-
-        <Link to="/" >
-          <button type="button" className="btn btn-warning">Go back to homepage</button>
-        </Link>
+      <div className="checkout-container">
+        <div className="confirmation-img-container d-flex justify-content-center">
+          <img src="/img/happy.svg" className="confirmation-img" alt="" />
+        </div>
+        <div className="container text-center py-4 confirmation-text">
+          <h4 className="fw-bold">
+            Your order is <span className="text-success">placed</span>!
+          </h4>
+          <p>Check your mail for confirmation</p>
+        </div>
+        <div className="container d-flex justify-content-center">
+          <Link to="/" >
+            <button type="button" className="btn-homepage">Go back to homepage</button>
+          </Link>
+        </div>
       </div>
     </>
   );
