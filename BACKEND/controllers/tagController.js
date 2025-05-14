f655dbb3662cddcca5f1b355ec9342cbadf86230
PirasSalvatore <torep.92@gmail.com>
2025-05-14 11:12:11 +0200

    feat: add 404 error handling for category and tag not found in controllers

--- BACKEND/controllers/tagController.js
+++ BACKEND/controllers/tagController.js
@@ -27,6 +27,7 @@ function getByName(req, res) {
 
     connection.query(sql, tagSlug, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.length === 0) return res.status(404).json({ error: 'Tag not found' })
 
         const tags = results[0]
         tags.image = `${url_base_image}${tags.image}`
