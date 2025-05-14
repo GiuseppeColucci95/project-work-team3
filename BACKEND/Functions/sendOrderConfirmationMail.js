1221d65339e11a5b3202c03c084566f85c9237bc
PirasSalvatore <torep.92@gmail.com>
2025-05-14 17:07:16 +0200

    feat: aggiungi invio email di conferma all'amministratore per gli ordini

--- BACKEND/Functions/sendOrderConfirmationMail.js
+++ BACKEND/Functions/sendOrderConfirmationMail.js
@@ -10,7 +10,7 @@ async function sendOrderConfirmationMail(order, orderId) {
             user: process.env.MAIL_USERNAME,
             pass: process.env.MAIL_PASSWORD
         }
-    });
+    })
 
     // Imposta i dettagli della mail
     let mailOptions = {
@@ -28,8 +28,24 @@ async function sendOrderConfirmationMail(order, orderId) {
         `
     }
 
+    let mailSeller = {
+        from: process.env.MAIL_USERNAME,
+        to: process.env.MAIL_USERNAME,
+        subject: 'Conferma ordine',
+        html: `
+            <div style="font-family: Arial, sans-serif; color: #333;">
+                <h2 style="color: #4CAF50;">Grazie per il tuo ordine!</h2>
+                <p>Ciao <strong>${order.firstname}</strong>,</p>
+                <p>Il tuo ordine <b>#${orderId}</b> è stato ricevuto con successo!</p>
+                <hr>
+                <p style="font-size: 0.9em; color: #888;">Ti contatteremo appena sarà spedito.</p>
+            </div>
+        `
+    }
+
     // Invia la mail
     await transporter.sendMail(mailOptions)
+    await transporter.sendMail(mailSeller)
 }
 
 module.exports = sendOrderConfirmationMail
