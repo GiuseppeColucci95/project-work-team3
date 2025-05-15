1338cd155791d4eded29050b67aeefe1392f949c
PirasSalvatore <torep.92@gmail.com>
2025-05-15 16:51:47 +0200

    fix: aggiungi gestione dello stato del body e migliora lo stile del pulsante nel carrello offcanvas

--- FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
+++ FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
@@ -1,7 +1,7 @@
 import "./OffcanvasCartStyle.css"
 import { useProductContext } from "../../contexts/ProductContext"
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 export default function OffcanvasCart({ setOffcanvasCartOpen, offcanvasCartOpen }) {
@@ -13,6 +13,16 @@ export default function OffcanvasCart({ setOffcanvasCartOpen, offcanvasCartOpen 
         setIsOpen(!isOpen)
         setOffcanvasCartOpen(!offcanvasCartOpen)
     }
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add("overflow-hidden")
+        } else {
+            document.body.classList.remove("overflow-hidden")
+        }
+        // Pulizia in caso di smontaggio
+        return () => document.body.classList.remove("overflow-hidden")
+    }, [isOpen])
 
 
     return (
@@ -34,23 +44,24 @@ export default function OffcanvasCart({ setOffcanvasCartOpen, offcanvasCartOpen 
                                         <img src={product.image} alt="image" className="w-100 rounded-4" />
                                     </div>
 
-                                    <div className="col-9 d-flex flex-column ">
+                                    <div className="col-8 d-flex flex-column ">
                                         <h6 className="p-0 product-name-cart">{product.name}</h6>
-                                        <div className="d-flex align-items-center gap-1 quantity-section ">
+
+                                        <div className="d-flex align-items-center gap-1 ">
                                             <button onClick={() => removeCartProduct(product)} className="offcanvas_cart-button"><i className="bi bi-dash-circle"></i></button>
-                                            <div id="offcanvas_cart-quantity" className="mx-1">{product.cartQuantity}</div>
+                                            <div id="offcanvas_cart-quantity" className="px-1">{product.cartQuantity}</div>
                                             <button onClick={() => addCartProduct(product)} className="offcanvas_cart-button"><i className="bi bi-plus-circle"></i></button>
                                         </div>
                                         {
                                             (product.discount_percentage > 0) ?
                                                 (
                                                     <div>
-                                                        <span className="fs-6 fw-semibold"><s>{`${(product.price * product.cartQuantity).toFixed(2)}€`}</s></span>
-                                                        <span className="fs-6 fw-semibold">{`${((product.price - product.price * (product.discount_percentage / 100)).toFixed(2) * product.cartQuantity).toFixed(2)}€`}</span>
+                                                        <span className="mt-2 px-2 fs-6 fw-semibold"><s>{`${(product.price * product.cartQuantity).toFixed(2)}€`}</s></span>
+                                                        <span className="mt-2 px-2 fs-6 fw-semibold">{`${((product.price - product.price * (product.discount_percentage / 100)).toFixed(2) * product.cartQuantity).toFixed(2)}€`}</span>
                                                     </div>
                                                 )
                                                 :
-                                                (<h3 className="fs-6 fw-semibold">{`${(product.price * product.cartQuantity).toFixed(2)}€`}</h3>)
+                                                (<h3 className="mt-2 px-2 fs-6 fw-semibold">{`${(product.price * product.cartQuantity).toFixed(2)}€`}</h3>)
                                         }
                                     </div>
                                 </div>
@@ -59,20 +70,22 @@ export default function OffcanvasCart({ setOffcanvasCartOpen, offcanvasCartOpen 
                     }
                 </div>
 
-                <div className="col offcanvas_cart-footer d-flex flex-column justify-content-center align-items-start">
-                    <div className="col offcanvas_cart-dettails">
+                <div className="col-2 offcanvas_cart-footer d-flex flex-column align-items-start pt-2">
+
+                    <div className="col offcanvas_cart-details">
                         <div className="offcanvas_cart-total d-flex justify-content-around">
                             <h6 className="offcanvas_cart-total-title">Shipping:</h6>
                             <p className="offcanvas_cart-total-price">&euro;{(totalPrice > 39.99) ? ('0.00') : ('9.99')}</p>
                         </div>
-                        <div className="offcanvas_cart-total d-flex justify-content-around">
+                        <div className="offcanvas_cart-shipping d-flex justify-content-around">
                             <h6 className="offcanvas_cart-total-title">Total:</h6>
                             <p className="offcanvas_cart-total-price">&euro;{totalPrice}</p>
                         </div>
                     </div>
-                    <div className="col align-self-center">
+
+                    <div className="col align-self-center ">
                         <Link to="/cart" >
-                            <button className="btn-checkout" onClick={() => setOffcanvasCartOpen(!offcanvasCartOpen)}>Go to Cart</button>
+                            <button className="offcanvas_cart_btn-cart mb-3" onClick={() => setOffcanvasCartOpen(!offcanvasCartOpen)}>Go to Cart</button>
                         </Link>
                     </div>
 
