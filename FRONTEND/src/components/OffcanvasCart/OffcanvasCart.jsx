70b1ef9f8a388d3770231739fdcfd23e134e185c
PirasSalvatore <torep.92@gmail.com>
2025-05-15 12:55:25 +0200

    ottimizza la gestione del carrello offcanvas con miglioramenti nella logica di apertura e chiusura

--- FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
+++ FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
@@ -1,15 +1,18 @@
 import "./OffcanvasCartStyle.css"
 
 
-export default function OffcanvasCart({ setOffcanvasCartOpen }) {
+export default function OffcanvasCart({ setOffcanvasCartOpen, offcanvasCartOpen }) {
     return (
-        <div className="offcanvas" tabIndex="-1" onClick={() => setOffcanvasCartOpen(false)}>
-            <div className="offcanvas-header">
-                <h5 className="offcanvas-title">Cart</h5>
-                <button type="button" className="btn-close" onClick={() => setOffcanvasCartOpen(false)}></button>
-            </div>
-            <div className="offcanvas-body">
-                {/* Cart items go here */}
+        <div className={offcanvasCartOpen ? "offcanvas_cart" : "offcanvas_cart hidden"} tabIndex="-1" >
+            <div className="offcanvas_cart_container">
+                <div className="offcanvas_cart-header">
+                    <h5 className="offcanvas_cart-title">Cart</h5>
+                    <button type="button" className="btn-close" onClick={() => setOffcanvasCartOpen(!offcanvasCartOpen)}></button>
+                </div>
+                <div className="offcanvas_cart-body">
+                    {/* Cart items go here */}
+                </div>
+
             </div>
         </div>
     )
