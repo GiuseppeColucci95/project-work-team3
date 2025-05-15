bab63a2371e49367f0831c27421733f6b642a27b
PirasSalvatore <torep.92@gmail.com>
2025-05-15 12:21:12 +0200

    riordina il componente OffcanvasCart rimuovendo codice non necessario e migliorando la leggibilità

--- FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
+++ FRONTEND/src/components/OffcanvasCart/OffcanvasCart.jsx
@@ -1,30 +1,16 @@
-//import "./offcanvasCartStyle.css"
+import "./OffcanvasCartStyle.css"
 
 
 export default function OffcanvasCart({ setOffcanvasCartOpen }) {
     return (
-
-        <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas" tabIndex="-1" onClick={() => setOffcanvasCartOpen(false)}>
             <div className="offcanvas-header">
-                <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <h5 className="offcanvas-title">Cart</h5>
+                <button type="button" className="btn-close" onClick={() => setOffcanvasCartOpen(false)}></button>
             </div>
             <div className="offcanvas-body">
-                <div>
-                    Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-                </div>
-                <div className="dropdown mt-3">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                        Dropdown button
-                    </button>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#">Action</a></li>
-                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                        <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
+                {/* Cart items go here */}
             </div>
         </div>
-
     )
 }
