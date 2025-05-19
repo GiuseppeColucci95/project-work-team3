95184a0c256c334193737fc85d052cc14aa5ec34
antonella1986 <antonella.leo.mail@gmail.com>
2025-05-19 11:56:49 +0200

    add loader video

--- FRONTEND/src/components/Loader.jsx
+++ FRONTEND/src/components/Loader.jsx
@@ -3,8 +3,33 @@ export default function Loader() {
 
   //template
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="spinner-border text-warning" role="status"></div>
+    <div className="d-flex justify-content-center align-items-center flex-column">
+      <h3 className="pb-5" style={{ color: "rgb(35, 25, 0)" }}>Your payment is being processed... 😌</h3>
+      <div
+        style={{
+          width: "400px",
+          height: "400px",
+          borderRadius: "50%",
+          overflow: "hidden",
+          boxShadow: "0 0 20px rgba(0,0,0,0.1)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+      <video
+        src="/video/payment-loader.mp4"
+        autoPlay
+        loop
+        muted
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "left" // aggiungi questa riga
+        }}
+      />
+      </div>
     </div>
   );
 }
